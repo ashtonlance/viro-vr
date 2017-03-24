6366a5526a01ef8fe81d6edd633c005e599c5ca2
Ashton Lance <ashton@ashtonlance.com>
2017-03-24 16:58:49 -0400

    Added in Timer Mixin
    Using this method until the Fuse button API is finished in Viro

--- js/HelloBeachScene.js
+++ js/HelloBeachScene.js
@@ -4,25 +4,33 @@ import React, { Component } from 'react';
 
 import {StyleSheet} from 'react-native';
 
+var TimerMixin = require('react-timer-mixin');
+
 import {
   ViroScene,
   Viro360Image,
   Viro360Video,
+  ViroBox,
+  ViroMaterials,
+  ViroCamera,
 } from 'react-viro';
 
 var HelloBeachScene = React.createClass({
+  mixins: [TimerMixin],
   getInitialState() {
     return {
     };
   },
   render: function() {
     return (
-     <ViroScene onClick={this._showHelloWorldScene}>
+     <ViroScene>
+     <ViroCamera position={[0, 0, 0]} active={true} />
      <Viro360Video
-       source={require("./video/CMC-360_0036_TEST-1_EDITED_03-23-17.mp4")}
+       source={require("./video/CMC-360_0036_TEST-Title-InsetVideo-1920x960_EDITED_03-23-17.mp4")}
        onFinish={this._onFinish}
        loop={true}
        />
+       <ViroBox position={[-4, -4, -2]} width={.5} height={.5} length={.5} scale={[1,1,1]} materials={["spacebox"]} onHover={this._showHelloWorldScene}/>
      </ViroScene>
 
 
@@ -30,9 +38,18 @@ var HelloBeachScene = React.createClass({
   },
 
   _showHelloWorldScene() {
-    this.props.sceneNavigator.pop();
+    this.setTimeout(
+    () => {this.props.sceneNavigator.push({scene:require("./HelloWorldScene.js")}); },
+   2000
+   );
   },
 
+});
+
+ViroMaterials.createMaterials({
+  spacebox: {
+    diffuseTexture: require('./res/360_space.jpg'),
+  },
 });
 
 module.exports = HelloBeachScene;
--- js/HelloWorldScene.js
+++ js/HelloWorldScene.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 
 import {StyleSheet} from 'react-native';
 
+var TimerMixin = require('react-timer-mixin');
+
 import {
   ViroScene,
   ViroText,
@@ -11,21 +13,26 @@ import {
   Viro360Video,
   ViroBox,
   ViroMaterials,
+  ViroAnimations,
+  ViroAnimatedComponent,
 } from 'react-viro';
 
 var HelloWorldScene = React.createClass({
+  mixins: [TimerMixin],
   getInitialState() {
     return {
-      text:"Hello World!",
+      text:"Hover over box for 2 seconds to launch video",
     };
   },
   render: function() {
     return (
      <ViroScene>
-
       <Viro360Image source={require('./res/360_space.jpg')} />
         <ViroText text={this.state.text} width={2} height={2} position={[0, 0, -2]} style={styles.helloWorldTextStyle} />
-        <ViroBox position={[0, -1, -2]} width={.5} height={.5} length={.5} scale={[1,1,1]} materials={["grid"]} onHover={this._showHelloBeachScene} onClick={this._showHelloBeachScene}/>
+        <ViroAnimatedComponent animation='animateImage' loop={true} run={true}>
+          <ViroBox position={[0, -2, -3]} width={.5} height={.5} length={.5} scale={[2,2,2]} materials={["grid"]}
+         onHover={this._showHelloBeachScene}/>
+         </ViroAnimatedComponent>
 
      </ViroScene>
     );
@@ -36,25 +43,28 @@ var HelloWorldScene = React.createClass({
    this.setState({
      text
    });
-  // if(isHovering) {
-  //     console.log("We are hovering onto the image!");
-  //     this.setState({
-  //       "Hello World!"
-  //     })
-  //   }else{
-  //     console.log("We are not longer hovering on the image!");
-  //   }
   },
 
  _showHelloBeachScene() {
-    this.props.sceneNavigator.push({scene:require("./HelloBeachScene.js")});
+   this.setTimeout(
+     () => {this.props.sceneNavigator.push({scene:require("./HelloBeachScene.js")}); },
+    2000
+    );
   },
+
+  // componentDidMount() {
+  //   this.setTimeout(
+  //     () => { console.log('I do not leak!'); },
+  //     500
+  //   );
+  // },
 });
+
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
     fontFamily: 'Arial',
-    fontSize: 60,
+    fontSize: 35,
     color: '#ffffff',
     textAlignVertical: 'center',
     textAlign: 'center',
@@ -65,6 +75,10 @@ ViroMaterials.createMaterials({
   grid: {
     diffuseTexture: require('./res/guadalupe_360.jpg'),
   },
+});
+
+ViroAnimations.registerAnimations({
+    animateImage:{properties:{rotateY:"+=50"}, duration:500},
 });
 
 module.exports = HelloWorldScene;
